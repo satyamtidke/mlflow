55482a82755de896422b0c1cd7f110ea17208ced
satyamtidke <56634268+satyamtidke@users.noreply.github.com>
2020-02-14 12:39:48 +0530

    Update mlflow_tracking.py

--- examples/quickstart/mlflow_tracking.py
+++ examples/quickstart/mlflow_tracking.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     log_metric("foo", random())
     log_metric("foo", random() + 1)
     log_metric("foo", random() + 2)
-    mlflow.set_experiment("my-experiment")
+    mlflow.set_experiment("my_experiment")
 
     if not os.path.exists("outputs"):
         os.makedirs("outputs")
