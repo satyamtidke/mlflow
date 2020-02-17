a1b9e2157ccf9eded73cafc6d1fc2255c06a861b
satyamtidke <56634268+satyamtidke@users.noreply.github.com>
2020-02-17 12:10:03 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import mlflow
-ML_TACKING_URI="http://10.77.36.45:5000/"
+ML_TRACKING_URI="http://10.77.36.45:5000/"
 MLFLOW_EXPERIMENT_NAME="mlflow_experiment_jnekins"
 mlflow.set_tracking_uri(ML_TRACKING_URI)
 mlflow.create_experiment(MLFLOW_EXPERIMENT_NAME, artifact_location=None)
