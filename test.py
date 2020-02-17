c7f40ca1592880c36a963d8b83083738c80e8736
satyamtidke <56634268+satyamtidke@users.noreply.github.com>
2020-02-17 12:18:54 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import mlflow
 import os
-print (os.environ["ML_TRACKING_URI"])
-ML_TRACKING_URI="http://10.77.36.45:5000/"
-MLFLOW_EXPERIMENT_NAME="mlflow_experiment_jnekins"
-mlflow.set_tracking_uri(ML_TRACKING_URI)
-mlflow.create_experiment(MLFLOW_EXPERIMENT_NAME, artifact_location=None)
+#print (os.environ["ML_TRACKING_URI"])
+#ML_TRACKING_URI="http://10.77.36.45:5000/"
+#MLFLOW_EXPERIMENT_NAME="mlflow_experiment_jnekins"
+mlflow.set_tracking_uri(os.getenv('ML_TRACKING_URI'))
+mlflow.create_experiment(os.getenv('MLFLOW_EXPERIMENT_NAME'), artifact_location=None)
 mlflow.start_run()
 mlflow.log_param("my", "param")
 mlflow.log_metric("score", 100)
