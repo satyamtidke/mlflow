642b2cb0508d644dc9c4b4bfbd8a4286f41940dc
satyamtidke <56634268+satyamtidke@users.noreply.github.com>
2020-02-17 12:04:51 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import mlflow
-mlflow.set_tracking_uri(ML_TRACKING_URI)
-mlflow.create_experiment(MLFLOW_EXPERIMENT_NAME, artifact_location=None)
+mlflow.set_tracking_uri("ML_TRACKING_URI")
+mlflow.create_experiment("MLFLOW_EXPERIMENT_NAME", artifact_location=None)
 mlflow.start_run()
 mlflow.log_param("my", "param")
 mlflow.log_metric("score", 100)
