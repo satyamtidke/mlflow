b83d9a760fc7d10e5ce927724df22f7a7f246af1
satyamtidke <56634268+satyamtidke@users.noreply.github.com>
2020-03-24 15:06:59 +0530

    tag creation file

---
+++ tag_create.py
@@ -0,0 +1,7 @@
+from  mlflow.tracking import MlflowClient
+client = MlflowClient()
+experiments = client.list_experiments() # returns a list of mlflow.entities.Experiment
+run = client.create_run(experiments[0].experiment_id) # returns mlflow.entities.Run
+client.log_param(run.info.run_id, "hello", "world")
+client.set_terminated(run.info.run_id)
+client.set_tag(run.info.run_id, "tag_key", "tag_value")
