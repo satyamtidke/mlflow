e3cfdf4f17dd3521a668867788cb7d3daad04244
satyamtidke <56634268+satyamtidke@users.noreply.github.com>
2020-03-25 16:30:24 +0530

    Update mlflow_tracking.py

--- examples/quickstart/mlflow_tracking.py
+++ examples/quickstart/mlflow_tracking.py
@@ -12,9 +12,9 @@ if __name__ == "__main__":
     log_metric("foo", random() + 1)
     log_metric("foo", random() + 2)
 
-    if not os.path.exists("outputs"):
-        os.makedirs("outputs")
-    with open("outputs/test.txt", "w") as f:
-        f.write("hello world!")
+#     if not os.path.exists("outputs"):
+#         os.makedirs("outputs")
+#     with open("outputs/test.txt", "w") as f:
+#         f.write("hello world!")
 
     log_artifacts("outputs")
