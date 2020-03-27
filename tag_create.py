9bf6bede6b9808f19c1614c680686aa5b418f7a3
satyamtidke <56634268+satyamtidke@users.noreply.github.com>
2020-03-27 16:26:31 +0530

    Update tag_create.py

--- tag_create.py
+++ tag_create.py
@@ -1,7 +1,11 @@
 from  mlflow.tracking import MlflowClient
+import mlflow
 import os
 client = MlflowClient()
 experiments = client.list_experiments() # returns a list of mlflow.entities.Experiment
+remote_server_uri = "http://10.77.36.45:5000/" # set to your server URI
+mlflow.set_tracking_uri(remote_server_uri)
+
 run = client.create_run(experiments[0].experiment_id) # returns mlflow.entities.Run
 client.log_param(run.info.run_id, "hello", "world")
 client.set_terminated(run.info.run_id)
