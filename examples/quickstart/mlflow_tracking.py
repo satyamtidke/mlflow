5703560a4d32844ee300b2c027fbeca68b251c0a
satyamtidke <56634268+satyamtidke@users.noreply.github.com>
2020-03-27 15:41:38 +0530

    Update mlflow_tracking.py

--- examples/quickstart/mlflow_tracking.py
+++ examples/quickstart/mlflow_tracking.py
@@ -12,8 +12,9 @@ if __name__ == "__main__":
     log_metric("foo", random())
     log_metric("foo", random() + 1)
     log_metric("foo", random() + 2)
-    mlflow.set_experiment(os.getenv('MLFLOW_EXPERIMENT_NAME'))
-   # mlflow.set_experiment("my_experiment")
+    print(os.getenv('MLFLOW_EXPERIMENT_NAME'))
+   # mlflow.set_experiment(os.getenv('MLFLOW_EXPERIMENT_NAME'))
+    mlflow.set_experiment("regression_module")
 
     if not os.path.exists("outputs"):
         os.makedirs("outputs")
