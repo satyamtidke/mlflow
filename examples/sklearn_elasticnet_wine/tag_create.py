68884dd998efb005a78d50716eb87e4741ef5f8d
satyamtidke <56634268+satyamtidke@users.noreply.github.com>
2020-04-02 14:49:08 +0530

    Update tag_create.py

--- examples/sklearn_elasticnet_wine/tag_create.py
+++ examples/sklearn_elasticnet_wine/tag_create.py
@@ -4,9 +4,9 @@ import os
 client = MlflowClient()
 experiments = client.list_experiments() # returns a list of mlflow.entities.Experiment
 remote_server_uri = "http://10.77.36.45:5050/" # set to your server URI
-mlflow.set_tracking_uri(remote_server_uri)
+mlflow.set_tracking_uri(os.getenv('MLFLOW_TRACKING_URI'))
 
-run = client.create_run(experiments[0].experiment_id) # returns mlflow.entities.Run
+run = client.create_run(3) # returns mlflow.entities.Run
 client.log_param(run.info.run_id, "hello", "world")
 client.set_terminated(run.info.run_id)
-client.set_tag(run.info.run_id,"36","new module")
+client.set_tag(run.info.run_id,os.getenv('tag_key'),"new module")
